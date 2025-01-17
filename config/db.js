ee30f59671ae2b87b2235d3e65cbcb282aa6b456
Erfan <erfanalaei2001@gmail.com>
2025-01-17 13:54:55 +0330

    Add Retry mechanism

--- config/db.js
+++ config/db.js
@@ -1,17 +1,23 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 const mongoURI = process.env.MONGO_URI;
 
 const connectDB = async () => {
+  let retries = 5;
+  while (retries) {
     try {
-        await mongoose.connect(mongoURI)
-        console.log('MongoDB Connected...');
+      await mongoose.connect(mongoURI);
+      console.log("MongoDB Connected...");
+      break;
+    } catch (err) {
+      console.log(err.message);
+      retries -= 1;
+      console.log(`Retries left: ${retries}`);
+      await new Promise((res) => setTimeout(res, 5000)); 
     }
-    catch (err) {
-        console.log(err.message)
-    }
-}
+  }
+};
 
 export default connectDB;
