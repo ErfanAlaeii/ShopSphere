4f9def1c55555cf9dd73bd5968a1babf6602c6d5
Erfan <erfanalaei2001@gmail.com>
2025-01-18 15:30:05 +0330

    API Rate Limiting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { apiLimiter } from './middlewares/rateLimiter.js';
 import userRoutes from './routes/userRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import errorHandler from './middlewares/errorMiddleware.js';
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/api/', apiLimiter);
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use(errorHandler);
